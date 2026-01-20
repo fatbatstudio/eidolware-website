74830b85faf9bd344b89473dd042b06914412316
Andy Wong <andy@inviteeducation.com>
2026-01-20 13:20:00 -0500

    saving current changes

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'EIDOLWARE';
-export const SITE_DESCRIPTION = 'Infiltrate a 24/7 anime convention powered by crypto, holograms, and delusional characters';
-export const BLUESKY_HANDLE = 'fatbat.studio';
+export const SITE_TITLE = "EIDOLWARE";
+export const SITE_DESCRIPTION =
+  "Infiltrate a 24/7 anime convention powered by holograms, AI, and a cast of eccentric characters";
+export const BLUESKY_HANDLE = "fatbat.studio";
